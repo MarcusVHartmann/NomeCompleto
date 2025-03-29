6017606304b2f7879064dc97033b26c546bca485
MarcusVHart <haykirengeral@gmail.com>
2025-03-29 16:26:34 -0300

    alteração de estrutura de codigo

--- Program.cs
+++ Program.cs
@@ -1,21 +1,14 @@
 ﻿string nome;
 string sobrenome;
 
-
 Console.Write("Digite seu primeiro nome: ");
 nome = Console.ReadLine()??"";
 
 Console.Write("Digite seu sobrenome: ");
 sobrenome = Console.ReadLine()??"";
 
-
-
 Console.Write("Seu nome é: ");
-
 Console.WriteLine($"{nome} {sobrenome}");
 
-
-
 Console.Write("Em catálogo fica: ");
-
 Console.WriteLine($"{sobrenome.ToUpper()}, {nome}");
