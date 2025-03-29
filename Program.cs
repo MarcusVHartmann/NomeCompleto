1ab116e039ea9331c71999dc0742e9818805de7a
MarcusVHart <haykirengeral@gmail.com>
2025-03-29 16:20:36 -0300

    criação de interação com nome, sobrenome e catálogo

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿string nome;
+string sobrenome;
+
+
+Console.Write("Digite seu primeiro nome: ");
+nome = Console.ReadLine()??"";
+
+Console.Write("Digite seu sobrenome: ");
+sobrenome = Console.ReadLine()??"";
+
+
+
+Console.Write("Seu nome é: ");
+
+Console.WriteLine($"{nome} {sobrenome}");
+
+
+
+Console.Write("Em catálogo fica: ");
+
+Console.WriteLine($"{sobrenome.ToUpper()}, {nome}");
